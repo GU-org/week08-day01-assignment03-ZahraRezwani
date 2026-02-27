1a8d0fdbd0fbfed4d86f046e38d30c067445a99f
Zahra Rezvani <zrarezvani1385.com@gmail.com>
2026-02-27 21:31:57 +0430

    changes

--- Week08/Day01/Assignment02/index.js
+++ Week08/Day01/Assignment02/index.js
@@ -3,12 +3,15 @@ function lifeInWeeks(age) {
 /************Don't change the code above************/    
     
     //Write your code here.
-   
-    
-    
-    
-    
-    
-/*************Don't change the code below**********/
+   let age = prompt("Enter your age:");
+
+let yearsLeft = 90 - age;
+let monthsLeft = yearsLeft * 12;
+let weeksLeft = yearsLeft * 52;
+
+alert("You have " + yearsLeft + " years, " +
+      monthsLeft + " months, and " +
+      weeksLeft + " weeks left.");
+    /*************Don't change the code below**********/
 }
 
